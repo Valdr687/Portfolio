8d1d0fc1c8c230af43f51a3f66b73843c27bcf5a
Thomas C <106614142+Valdr687@users.noreply.github.com>
2022-08-04 21:57:03 +0200

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  $(".image").click(function(){
+    $(this).toggleClass("image-actif");
+    $("img").toggle();
+    $(this).children('img').show()
+    $('*').toggleClass("barre-cache");
+    $('body').toggleClass("body-image");
+  });
+});
+$(document).ready(function(){
+  $(".hamburger").click(function(){
+    $(this).toggleClass("is-active");
+    $('body').toggleClass("menu");
+
+  });
+});
+
+
+
+
+
+
