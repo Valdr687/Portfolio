4eb47346f7704268fd3ac543e5db28c71a12dff7
Valdr687 <106614142+Valdr687@users.noreply.github.com>
2023-03-04 17:38:58 +0100

    Réécriture de quotes

--- js/quotes.js
+++ js/quotes.js
@@ -1,56 +1,26 @@
-var TxtRotate = function(el, toRotate, period) {
-    this.toRotate = toRotate;
-    this.el = el;
-    this.loopNum = 0;
-    this.period = parseInt(period, 10) || 2000;
-    this.txt = '';
-    this.tick();
-    this.isDeleting = false;
-  };
-  
-  TxtRotate.prototype.tick = function() {
-    var i = this.loopNum % this.toRotate.length;
-    var fullTxt = this.toRotate[i];
-  
-    if (this.isDeleting) {
-      this.txt = fullTxt.substring(0, this.txt.length - 1);
-    } else {
-      this.txt = fullTxt.substring(0, this.txt.length + 1);
-    }
-  
-    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-  
-    var that = this;
-    var delta = 300 - Math.random() * 100;
-  
-    if (this.isDeleting) { delta /= 2; }
-  
-    if (!this.isDeleting && this.txt === fullTxt) {
-      delta = this.period;
-      this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === '') {
-      this.isDeleting = false;
-      this.loopNum++;
-      delta = 500;
-    }
-  
-    setTimeout(function() {
-      that.tick();
-    }, delta);
-  };
-  
-  window.onload = function() {
-    var elements = document.getElementsByClassName('txt-rotate');
-    for (var i=0; i<elements.length; i++) {
-      var toRotate = elements[i].getAttribute('data-rotate');
-      var period = elements[i].getAttribute('data-period');
-      if (toRotate) {
-        new TxtRotate(elements[i], JSON.parse(toRotate), period);
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+quotes = [ "lowpoly artist.", "phototaker.", "frontend enthusiast."];
+
+async function displayQuotes(tab,delay) {
+  for (let i in tab) {
+    var quoteDisplayed = tab[i];
+    var word = "";
+    for (let j in quoteDisplayed) {
+      if (quoteDisplayed[j] == " ") {
+        await sleep(delay/2);
       }
+      else {
+        await sleep(delay);
+      }
+      word += quoteDisplayed[j];
+      $("#txt-rotate").text(word);
+      //console.log(word) //Le commentaire permet de ne pas polluer la console
     }
-    // INJECT CSS
-    var css = document.createElement("style");
-    css.type = "text/css";
-    css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
-    document.body.appendChild(css);
-  };
+    await sleep(delay*2);
+  }
+  displayQuotes(tab,delay);
+}
+displayQuotes(quotes,200);
