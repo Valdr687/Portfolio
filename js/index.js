323952762a2daf5b4007588258d084c61c1a2874
Valdr687 <106614142+Valdr687@users.noreply.github.com>
2023-03-04 16:58:58 +0100

    Sauvegarde

--- js/index.js
+++ js/index.js
@@ -1,32 +1,53 @@
-//Variables 
+//Variables & fonctions
 var NbrTelechargementPMC = 4_233;
-var DownloadThreshold = {"4000": "four thousands","5000": "five thousands","6000": "six thousands",
+DownloadThreshold = {"4000": "four thousands","5000": "five thousands","6000": "six thousands",
 "7000": "seven thousands","8000": "eight thousands","9000": "eight thousands","10000": "ten thousands",
 "15000": "fifteen thousands","20000": "twenty thousands",}
 NombreImage=7
 ConfigImage=[]
 AltImage=[]
 
-// API 
-/*
-$(document).ready(function() {
-  $.ajax({
-    url: "https://api.modrinth.com/v2/project/LBcosBrl",
-    type: "GET",
-    dataType: "json",
-    success: function(result) {
-      var data = result;
-      console.log("Nombre de téléchargements Modrinth" + data.downloads )
-      $("#res").text(textString);
-    },
-    error: function(error) {
-      console.log(error);
-    }
-  });
-});
-*/
+function getKey(number) {
+  number = Math.floor(number/1000)*1000;
+  if (number <= 10_000) {
+    return number.toString() ;
+  }
+  else if ( number <= 15_000) {
+    return "15000"
+  }
+  else if ( number <= 20_000) {
+    return "20000"
+  }
+}
 
-// Carousel 
+// API 
+
+function getData(id, callback) {
+  $.ajax({
+      type: "GET",
+      url: "https://api.modrinth.com/v2/project/"+id,
+      dataType: "json",
+      success: callback,
+      error: function(error) {
+        console.log(error);
+      }
+  });
+}
+
+getData(id, function(response) {
+    return response.downloads ;
+});
+
+var downloads = NbrTelechargementPMC + getDownloads('LBcosBrl')
+
+//Insertion des variables
+
+$(document).ready(function () {
+  $("#id").text(DownloadThreshold[getKey(downloads)]);
+});
+
+// Carousel
+
 for (let i=0;i<NombreImage;i++) {
   ConfigImage.push(i+1)
 }
@@ -58,7 +79,7 @@ $(document).ready(function () {
 });
 
 
-//Insertion des variables
+
 
 
 //Menu
